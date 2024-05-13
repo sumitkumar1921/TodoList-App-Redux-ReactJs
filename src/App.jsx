72c89e19ec56a9afd7932ea5199b30bc94783850
Sumit Kumar <sumitkumar192113920@gmail.com>
2024-05-13 22:59:19 +0530

    Complete Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn About Redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  );
+}
+
+export default App;
